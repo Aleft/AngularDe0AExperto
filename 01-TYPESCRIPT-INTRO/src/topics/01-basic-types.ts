2ed280f53de9be1bef01997e85540abbbb8195f5
Alexandros Diaz <alebmxft@gmail.com>
2024-05-23 17:18:41 -0300

    Initial commit

---
+++ 01-TYPESCRIPT-INTRO/src/topics/01-basic-types.ts
@@ -0,0 +1,17 @@
+let name: string = 'strider'; 
+let hpPoints: number | string = 95;
+let hpPoints2: number | 'full' = 95;
+const isAlive: boolean = true;
+
+
+hpPoints = 'full'; 
+
+hpPoints2 = 100;
+console.log(
+    name,
+    hpPoints,
+    hpPoints2,
+    isAlive
+)
+
+export {};
