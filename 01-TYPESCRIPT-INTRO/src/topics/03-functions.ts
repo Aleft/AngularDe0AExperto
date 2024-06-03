c549fc96520f374ebe10be9012185aabe853096b
Aleft <alebmxft@gmail.com>
2024-06-03 01:40:23 -0300

    Funciones con objetos como argumentos

--- 01-TYPESCRIPT-INTRO/src/topics/03-functions.ts
+++ 01-TYPESCRIPT-INTRO/src/topics/03-functions.ts
@@ -12,11 +12,36 @@ function multiply (firstNumber: number, secondNumber?: number, base = 2) {
     return firstNumber * base
 }
     
-
-const result:number = addNumbers(1,2);
-const result2:string = addNumbersArrow(1,2);
-const multiplyResult:number = multiply(5);
-console.log(result, result2, multiplyResult);
+// const result:number = addNumbers(1,2);
+// const result2:string = addNumbersArrow(1,2);
+// const multiplyResult:number = multiply(5);
+// console.log(result, result2, multiplyResult);
 // console.log(result2);
+
+//Video Funciones con objetos como argumentos
+
+interface Character {
+    name: string;
+    hp: number;
+    showHp: () => void;
+    // skills: string[];
+    // hometown?: string; //Poniendo el signo de pregunta despues de la variable me dice que la variable es opcional.
+}
+const healCharacter = (character: Character, amount: number) => {
+    
+    character.hp += amount; 
+} 
+
+const strider: Character = {
+    name: 'strider',
+    hp: 50,
+    showHp() {
+        console.log(`HP: ${this.hp}`);
+    }
+    // skills: ['Bash', 'Counter', 'Healing']
+    // hometown: 'New York'
+}
+healCharacter(strider, 10);
+strider.showHp();
 
 export {};
