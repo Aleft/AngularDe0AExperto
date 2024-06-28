13942b99be87c71a95bcff134c119ab69ae263e6
Alexandros Diaz <alebmxft@gmail.com>
2024-06-28 11:57:07 -0300

    optional chances

--- 01-TYPESCRIPT-INTRO/src/topics/11-optional.chaining.ts
+++ 01-TYPESCRIPT-INTRO/src/topics/11-optional.chaining.ts
@@ -13,7 +13,7 @@ const passanger2: Passanger = {
 }
 
 const returnChildNumber = (passenger: Passanger): number => {
-    const howManyChildren = passenger.children?.length || 0;
+    const howManyChildren = passenger.children?.length || 0; //Forma practica para evitar el if 
     console.log(`The passenger ${passenger.name} has ${howManyChildren} children.`);
     // console.log(passenger.name, howManyChildren)
     return howManyChildren;
