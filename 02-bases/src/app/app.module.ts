c279cadfab85d91d941706681539861e896ad5c5
Aleft <alebmxft@gmail.com>
2024-08-12 18:14:00 -0300

    Fin seccion 4

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -3,21 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
 import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent,
-    ListComponent,
+    // HeroComponent,
+    // ListComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CounterModule,
+    HeroesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
